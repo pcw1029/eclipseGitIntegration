4eff12622cf4ba5b16ca7d00466ed36baa83cde5
pcw1029 <pcw1029@gmail.com>
2023-06-02 13:04:40 +0900

    eclipse로 생성한 프로젝트와 Github연동 시험

---
+++ projectIntegrationWithGithub/src/projectIntegrationWithGithub.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : projectIntegrationWithGithub.cpp
+// Author      : pcw1029
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
